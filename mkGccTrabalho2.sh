a14dfccc8b82ca5c00d3c9e9709eb7257b07e76a
Gustavo Haas <41526823+gustavokisth@users.noreply.github.com>
2022-11-27 12:11:47 -0300

    Makefile Trabalho 2

--- mkGccTrabalho2.sh
+++ mkGccTrabalho2.sh
@@ -19,6 +19,7 @@ gcc -O3 -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include 
 ./ExecucaoComOpenmp 2
 ./ExecucaoComOpenmp 3
 ./ExecucaoComOpenmp 4
+./ExecucaoComOpenmp 0
 
 echo "compilando arquivo OpenMp com Gcc -Os"
 gcc -Os -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComOpenmp -lm
