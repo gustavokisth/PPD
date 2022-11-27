21a58c09b56d8fc21f11b986859cbe7faddf79e8
Gustavo Haas <41526823+gustavokisth@users.noreply.github.com>
2022-11-27 12:04:40 -0300

    Makefile Trabalho 2

--- mkGccTrabalho2.sh
+++ mkGccTrabalho2.sh
@@ -1,3 +1,9 @@
+cd /CMSIS_5/CMSIS/NN/Examples/ARM/arm_nn_examples/cifar10
+
+echo "Apagando arquivos antigos ..."
+rm ./Execucao* || true
+
+
 echo "compilando arquivo OpenMp com Gcc -O0"
 gcc -O0 -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComOpenmp -lm
 
@@ -13,7 +19,6 @@ gcc -O3 -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include 
 ./ExecucaoComOpenmp 2
 ./ExecucaoComOpenmp 3
 ./ExecucaoComOpenmp 4
-./ExecucaoComOpenmp 0
 
 echo "compilando arquivo OpenMp com Gcc -Os"
 gcc -Os -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComOpenmp -lm
@@ -23,24 +28,24 @@ gcc -Os -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include 
 ./ExecucaoComOpenmp 3
 ./ExecucaoComOpenmp 4
 
-echo "compilando arquivo MPI com Gcc -O0"
-mpicc -O0 -D_MPI -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
+echo "compilando arquivo MPI com MPIcc -O0"
+mpicc -O0 -D_MPI -w -g -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
 
 mpiexec -n 1 ./ExecucaoComMPI
 mpiexec -n 2 ./ExecucaoComMPI
 mpiexec -n 3 ./ExecucaoComMPI
 mpiexec -n 4 ./ExecucaoComMPI
 
-echo "compilando arquivo MPI com Gcc -O3"
-mpicc -O3 -D_MPI -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
+echo "compilando arquivo MPI com MPIcc -O3"
+mpicc -O3 -D_MPI -w -g -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
 
 mpiexec -n 1 ./ExecucaoComMPI
 mpiexec -n 2 ./ExecucaoComMPI
 mpiexec -n 3 ./ExecucaoComMPI
 mpiexec -n 4 ./ExecucaoComMPI
 
-echo "compilando arquivo MPI com Gcc -Os"
-mpicc -Os -D_MPI -g -w -fopenmp -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
+echo "compilando arquivo MPI com MPIcc -Os"
+mpicc -Os -D_MPI -w -g -I/CMSIS_5/CMSIS/DSP/Include -I/CMSIS_5/CMSIS/NN/Include -I/CMSIS_5/CMSIS/Core/Include /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_RGB.c /CMSIS_5/CMSIS/NN/Source/ActivationFunctions/arm_relu_q7.c /CMSIS_5/CMSIS/NN/Source/ConvolutionFunctions/arm_convolve_HWC_q7_fast.c /CMSIS_5/CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c /CMSIS_5/CMSIS/NN/Source/FullyConnectedFunctions/arm_fully_connected_q7_opt.c  /CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC.c arm_nnexamples_cifar10.c -o ExecucaoComMPI -lm
 
 mpiexec -n 1 ./ExecucaoComMPI
 mpiexec -n 2 ./ExecucaoComMPI
